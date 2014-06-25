cc85d952d4b760f755beb6fafb07ae9966e71350
Simon Peters <simonpeters3@gmail.com>
2014-06-25 20:10:10 +0200

    ANIMATIONZZZZ

--- main.js
+++ main.js
@@ -18,13 +18,14 @@ var ctx = canvas.getContext("2d");
 
 //create images
 var vinnie = new Image();
-vinnie.src = "vincent.jpg";
+vinnie.src = "vincent.gif";
 
 var imageArray = [];
 
 var spawn1 = {
 	x : 200,
-	y : 200
+	y : 200,
+    image: "vincent.gif"
 }
 
 var spawn2 = {
@@ -32,29 +33,56 @@ var spawn2 = {
 	y : 200
 }
 
+var spawn3 = {
+    x : 200,
+    y : 200
+}
+
 function render(){
 	ctx.clearRect(0, 0, width, height);
-	var image = {
-		x : spawn1.x,
-		y : spawn1.y,
-		dirX : Math.random() *10 - 5,
-		dirY : Math.random() *10 - 5
-	};
 
-	imageArray.push(image);
 
-	var image = {
-		x : spawn2.x,
-		y : spawn2.y,
-		dirX : Math.random() * 10 - 5,
-		dirY : Math.random() * 10 - 5
-	};
-	imageArray.push(image);
+
+    if(Math.floor(randomNumber(0,3)) != 1 || imageArray.length < 900){
+        var image = {
+            x : spawn1.x,
+            y : spawn1.y,
+            size: 0.1,
+            dirX : Math.random() *10 - 5,
+            dirY : Math.random() *10 - 8
+        };
+
+        imageArray.unshift(image);
+
+        var image = {
+            x : spawn2.x,
+            y : spawn2.y,
+            size : 0.1,
+            dirX : Math.random() * 10 - 5,
+            dirY : Math.random() * 10 - 6
+        };
+        //imageArray.unshift(image);
+
+        var image = {
+            x : spawn3.x,
+            y : spawn3.y,
+            size : 0.1,
+            dirX : Math.random() * 10 - 5,
+            dirY : Math.random() * 10 - 6
+        };
+        //imageArray.unshift(image);
+    }
+
+
 
 	imageArray.forEach(function(image){
 		image.x += image.dirX;
 		image.y  += image.dirY;
-		ctx.drawImage( vinnie , image.x , image.y , 40 , 40 );
+        image.dirY = image.dirY + 0.05;
+
+        if(image.size < 1 && randomNumber(1,1000 ) > 800) image.size += 0.05;
+
+		ctx.drawImage( vinnie , image.x , image.y , 50 * image.size , 60 * image.size );
 	});
 
     if(Math.round(Math.random() *50) == 1){
@@ -65,11 +93,16 @@ function render(){
     if(Math.round(Math.random() * 50) == 2){
         spawn2.x = randomNumber(50, width- 50);
         spawn2.y = randomNumber(50, height- 50);
-        console.log(height);
     }
 
-	if(imageArray.length > 2000) imageArray.shift();
-	if(imageArray.length > 2000) imageArray.shift();
+    if(Math.round(Math.random() * 50) == 3){
+        spawn3.x = randomNumber(50, width- 50);
+        spawn3.y = randomNumber(50, height- 50);
+    }
+
+	if(imageArray.length > 2000) imageArray.pop();
+	if(imageArray.length > 2000) imageArray.pop();
+	if(imageArray.length > 2000) imageArray.pop();
 
 
 }
